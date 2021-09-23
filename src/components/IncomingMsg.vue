3f782493aa43973c5a3d71a38b389d828b8fc7d8
Handson Panggabean <hansenpanggabean8@gmail.com>
2021-09-23 08:33:47 +0700

    updating a bit styling on chat room socket io and apidoc for nodemailer

--- src/components/IncomingMsg.vue
+++ src/components/IncomingMsg.vue
@@ -1,14 +1,30 @@
 <template>
-  <div class="incoming_msg pr-100">
-    <div class="received_msg" v-if="message.username != user.username">
-      <span class="time_date">
-        {{ message.username }}: {{ message.message }}
-      </span>
+  <div>
+    <div v-if="message.username == user.username" class="pl-80 items-end">
+      <div>
+        <div>
+          <div class="incoming_msg">
+            <div class="received_msg">
+              <span class="time_date">
+                {{ message.message }} : (Me)
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="received_msg" v-else>
-      <span class="time_date">
-        {{ message.username }}: {{ message.message }}
-      </span>
+    <div v-else class="pr-80" >
+      <div>
+        <div>
+          <div class="incoming_msg ">
+            <div class="received_msg">
+              <span class="time_date">
+                {{ message.username }}: {{ message.message }}
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
